40a0f1fbbba06bf5c49201aca50a4ff35cdadcb8
Weixiang Yu <wy73@drexel.edu>
2020-09-04 15:39:56 -0400

    add genral carma fit

--- src/fit.py
+++ src/fit.py
@@ -20,6 +20,7 @@ def neg_ll(params, y, yerr, gp):
     np.seterr(under=notify_method)
 
     params = np.array(params)
+    dim = len(params)
     run = True
     lap = 0
 
@@ -33,10 +34,10 @@ def neg_ll(params, y, yerr, gp):
             neg_ll = -gp.log_likelihood(y)
             run = False
         except celerite.solver.LinAlgError:
-            params += 1e-6 * np.random.randn(4)
+            params += 1e-6 * np.random.randn(dim)
             continue
         except np.linalg.LinAlgError:
-            params += 1e-6 * np.random.randn(4)
+            params += 1e-6 * np.random.randn(dim)
             continue
         except FloatingPointError:
             return -np.inf
@@ -202,10 +203,7 @@ def dho_log_param_init():
 def dho_fit(lc_df, debug=False, plot=False, bounds=None):
 
     best_fit = np.zeros((4, 6))
-
-    # fail_num = 0
     lc_df = lc_df.copy()
-    # std = np.std(lc_df.flux.values)
 
     if bounds is not None:
         first_bounds = bounds
@@ -222,6 +220,9 @@ def dho_fit(lc_df, debug=False, plot=False, bounds=None):
             yerr = lc_band.flux_err.values
 
             rerun = True  # dynamic control of bounds
+            compute = True  # handle can't factorize in gp.compute()
+            succeded = False  # ever succeded
+            compute_ct = 0
             counter = 0
             bounds = first_bounds
             jac_log_rec = 10
@@ -229,7 +230,15 @@ def dho_fit(lc_df, debug=False, plot=False, bounds=None):
             # initialize parameter, kernel and GP
             kernel = DHO_term(*dho_log_param_init())
             gp = GP(kernel, mean=np.mean(y))
-            gp.compute(t, yerr)
+
+            # compute can't factorize, try 4 more times
+            while compute & (compute_ct < 5):
+                compute_ct += 1
+                try:
+                    gp.compute(t, yerr)
+                    compute = False
+                except celerite.solver.LinAlgError:
+                    gp.set_parameter_vector(dho_log_param_init())
 
             # set bound based on LC std for amp
             while rerun and (counter < 5):
@@ -239,6 +248,7 @@ def dho_fit(lc_df, debug=False, plot=False, bounds=None):
                 )
 
                 if r.success:
+                    succeded = True
                     best_fit[:, band] = np.exp(r.x)
 
                     if "jac" not in r.keys():
@@ -261,7 +271,7 @@ def dho_fit(lc_df, debug=False, plot=False, bounds=None):
                     gp.set_parameter_vector(dho_log_param_init())
 
             # if no success found, set best to nan
-            if not r.success:
+            if not succeded:
                 best_fit[:, band] = np.nan
 
         except Exception as e:
@@ -271,7 +281,6 @@ def dho_fit(lc_df, debug=False, plot=False, bounds=None):
                 f"Exception at object_id: {lc_df.object_id.values[0]}, passband: {band}"
             )
             best_fit[:, band] = np.nan
-            # fail_num += 1
 
         # Below code is used to visualize if stuck in local minima
         if debug:
@@ -279,6 +288,145 @@ def dho_fit(lc_df, debug=False, plot=False, bounds=None):
 
         if plot:
             plot_dho_ll(t, y, yerr, np.exp(r.x), gp, vec_neg_ll)
+
+    return np.concatenate([[lc_df.object_id.values[0]], best_fit.flatten()])
+
+
+def carma_param_init(dim):
+    """
+    Randomly generate DHO parameters from [-8, 1] in log.
+
+    Args:
+        dim (int): For a CARMA(p,q_ model, dim=p+q+1
+
+    Returns:
+        list: The generated CAMRA parameters in natural log.
+    """
+
+    log_param = np.random.uniform(-8, 1, dim)
+
+    return log_param
+
+
+def carma_fit(lc_df, p, q, de=True, debug=False, plot=False, bounds=None):
+
+    best_fit = np.zeros((int(p + q + 1), 6))
+    lc_df = lc_df.copy()
+
+    if bounds is not None:
+        first_bounds = bounds
+    else:
+        first_bounds = [(-10, 1)] * int(p + q + 1)
+
+    # loop through lc in each passband
+    for band in range(6):
+
+        try:
+            lc_band = lc_df[lc_df.passband == band].copy()
+            t = lc_band.mjd.values - lc_band.mjd.min()
+            y = lc_band.flux.values
+            yerr = lc_band.flux_err.values
+
+            rerun = True  # dynamic control of bounds
+            compute = True  # handle can't factorize in gp.compute()
+            succeded = False  # ever succeded
+            compute_ct = 0
+            counter = 0
+            bounds = first_bounds
+            jac_log_rec = 10
+
+            # initialize parameter, kernel and GP
+            log_params = carma_param_init(int(p + q + 1))
+            kernel = CARMA_term(log_params[:p], log_params[p:])
+            gp = GP(kernel, mean=np.mean(y))
+
+            # compute can't factorize, try 4 more times
+            while compute & (compute_ct < 5):
+                compute_ct += 1
+                try:
+                    gp.compute(t, yerr)
+                    compute = False
+                except celerite.solver.LinAlgError:
+                    gp.set_parameter_vector(carma_param_init(int(p + q + 1)))
+
+            if de:
+                # set bound based on LC std for amp
+                while rerun and (counter < 5):
+                    counter += 1
+                    r = differential_evolution(
+                        neg_ll, bounds=bounds, args=(y, yerr, gp), maxiter=200
+                    )
+
+                    if r.success:
+                        succeded = True
+                        best_fit[:, band] = np.exp(r.x)
+
+                        if "jac" not in r.keys():
+                            rerun = False
+                        else:
+                            jac_log = np.log10(np.dot(r.jac, r.jac) + 1e-8)
+
+                            # if positive jac, then increase bounds
+                            if jac_log > 0:
+                                bounds = [(x[0] - 1, x[1] + 1) for x in bounds]
+                            else:
+                                rerun = False
+
+                            # update best-fit if smaller jac found
+                            if jac_log < jac_log_rec:
+                                jac_log_rec = jac_log
+                                best_fit[:, band] = np.exp(r.x)
+                    else:
+                        bounds = [(x[0] - 1, x[1] + 1) for x in bounds]
+                        gp.set_parameter_vector(carma_param_init(int(p + q + 1)))
+
+            else:
+                initial_params = gp.get_parameter_vector()
+
+                while rerun and (counter < 5):
+                    counter += 1
+                    r = minimize(
+                        neg_ll,
+                        initial_params,
+                        method="L-BFGS-B",
+                        bounds=bounds,
+                        args=(y, yerr, gp),
+                    )
+                    if r.success:
+                        succeded = True
+                        best_fit[:, band] = np.exp(r.x)
+                        jac_log = np.log10(np.dot(r.jac, r.jac) + 1e-8)
+
+                        # if positive jac, then increase bounds
+                        if jac_log > 0:
+                            bounds = [(x[0] - 1, x[1] + 1) for x in bounds]
+                        else:
+                            rerun = False
+
+                        # update best-fit if smaller jac found
+                        if jac_log < jac_log_rec:
+                            jac_log_rec = jac_log
+                            best_fit[:, band] = np.exp(r.x)
+                    else:
+                        bounds = [(x[0] - 1, x[1] + 1) for x in bounds]
+                        gp.set_parameter_vector(carma_param_init(int(p + q + 1)))
+
+            if not succeded:
+                best_fit[:, band] = np.nan
+
+        except Exception as e:
+            print(e)
+            print(
+                f"Exception at object_id: {lc_df.object_id.values[0]}, passband: {band}"
+            )
+            best_fit[:, band] = np.nan
+
+        # Below code is used to visualize if stuck in local minima
+        if debug:
+            print(r)
+
+    #         if plot:
+    #             plot_dho_ll(t, y, yerr, np.exp(r.x), gp, vec_neg_ll)
 
     return np.concatenate([[lc_df.object_id.values[0]], best_fit.flatten()])
 
